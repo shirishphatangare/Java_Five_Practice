8a0cf36452a4a0db3f586cb5db74e6c786f24d7e
shirishphatangare <36280454+shirishphatangare@users.noreply.github.com>
2018-10-16 21:33:45 -0500

    Updated code to include synchronizedMap
    Updated code to include synchronizedMap

--- ConcurrentHashMapTest.java
+++ ConcurrentHashMapTest.java
@@ -1,5 +1,7 @@
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
 
 
 
@@ -7,23 +9,24 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConcurrentHashMapTest {
 
 	public static void main(String[] args) {
-		ConcurrentHashMap<String,String> hashMap = new ConcurrentHashMap<String,String>();
+		//ConcurrentHashMap<String,String> conHashMap = new ConcurrentHashMap<String,String>();
 		
-		//HashMap<String,String> hashMap = new HashMap<String,String>();
+		HashMap<String,String> hashMap = new HashMap<String,String>();
+		Map<String,String> syncHashMap = Collections.synchronizedMap(hashMap);
 		
-		hashMap.put("1", "1");
-		hashMap.put("2", "2");
-		hashMap.put("3", "3");
-		hashMap.put("4", "4");
+		syncHashMap.put("1", "1");
+		syncHashMap.put("2", "2");
+		syncHashMap.put("3", "3");
+		syncHashMap.put("4", "4");
 		
-		Iterator<String> it = hashMap.keySet().iterator();
+		Iterator<String> it = syncHashMap.keySet().iterator();
 		
 		while(it.hasNext()){
-			hashMap.put("4", "44"); // No ConcurrentModificationException even for normal HashMap, since we are not changing structure of Hashmap. Just update to existing value
-			hashMap.put("5", "44"); // ConcurrentModificationException for normal HashMap but not for ConcurrentHashMap
+			syncHashMap.put("4", "44"); // No ConcurrentModificationException even for normal HashMap and synchronizedMap, since we are not changing structure of Hashmap. Just update to existing value
+			syncHashMap.put("5", "44"); // ConcurrentModificationException for normal HashMap and synchronizedMap but not for ConcurrentHashMap
 			System.out.println(it.next());
 		}
-		System.out.println(hashMap);
+		System.out.println(syncHashMap);
 
 	}
 
