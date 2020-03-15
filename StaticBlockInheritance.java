5ee34ce04e0c798a62e816b2c5831031ec44556b
shirishphatangare <sphatangare@gmail.com>
2020-03-15 11:00:54 -0500

    Added comments in StaticBlockInheritance.java

--- StaticBlockInheritance.java
+++ StaticBlockInheritance.java
@@ -5,23 +5,23 @@ class A1 {
 	
 	// static block
 	static {
-		System.out.println("First static block i before operation " + i);
+		System.out.println("First static block i before operation " + i); // i = 100
 		
 		i = i-- - --i; // i = 100 - 98 = 2
 		
-		System.out.println("First static block i after operation " + i);
+		System.out.println("First static block i after operation " + i); // i = 2
 	}
 	
     // Normal block
 	{
-		System.out.println("Second normal block i before operation " + i);
-		System.out.println("Second normal block j before operation " + j);
+		System.out.println("Second normal block i before operation " + i); // i = 0
+		System.out.println("Second normal block j before operation " + j); // j = 200
 		
 		i = i++ + ++i; // i = 0 + 2 = 2 
 		j = j++ + ++j;  // j = 200 + 202 = 402
 		
-		System.out.println("Second normal block i after operation " + i);
-		System.out.println("Second normal block j after operation " + j);
+		System.out.println("Second normal block i after operation " + i); // i = 2
+		System.out.println("Second normal block j after operation " + j); // j = 402
 	}
 	
 }
@@ -30,23 +30,23 @@ class B1 extends A1 {
 	
 	// static block
 	static {
-		System.out.println("Third static block i before operation " + i);
+		System.out.println("Third static block i before operation " + i); // i = 2
 		
 		i = --i - i--; // i = 1 - 1 = 0
 		
-		System.out.println("Third static block i after operation " + i);
+		System.out.println("Third static block i after operation " + i); // i = 0
 	}
 	
     // Normal block
 	{
-		System.out.println("Forth normal block i before operation " + i);
-		System.out.println("Forth normal block j before operation " + j);
+		System.out.println("Forth normal block i before operation " + i); // i = 2
+		System.out.println("Forth normal block j before operation " + j); // j = 402
 		
 		i = ++i + i++; // i = 3 + 3 = 6
 		j = ++j + j++; // j = 403 + 403 = 806
 		
-		System.out.println("Forth normal block i after operation " + i);
-		System.out.println("Forth normal block j after operation " + j);
+		System.out.println("Forth normal block i after operation " + i); // i = 6
+		System.out.println("Forth normal block j after operation " + j); // j = 806
 	}
 	
 }
