afb17b3bb1ca58f045e11b3acb0e7264f49d1bec
shirishphatangare <sphatangare@gmail.com>
2020-03-15 10:22:15 -0500

    Added comments in StaticBlockInheritance.java

--- StaticBlockInheritance.java
+++ StaticBlockInheritance.java
@@ -7,7 +7,7 @@ class A1 {
 	static {
 		System.out.println("First static block i before operation " + i);
 		
-		i = i-- - --i;
+		i = i-- - --i; // i = 100 - 98 = 2
 		
 		System.out.println("First static block i after operation " + i);
 	}
@@ -17,8 +17,8 @@ class A1 {
 		System.out.println("Second normal block i before operation " + i);
 		System.out.println("Second normal block j before operation " + j);
 		
-		i = i++ + ++i;
-		j = j++ + ++j;
+		i = i++ + ++i; // i = 0 + 2 = 2 
+		j = j++ + ++j;  // j = 200 + 202 = 402
 		
 		System.out.println("Second normal block i after operation " + i);
 		System.out.println("Second normal block j after operation " + j);
@@ -32,7 +32,7 @@ class B1 extends A1 {
 	static {
 		System.out.println("Third static block i before operation " + i);
 		
-		i = --i - i--;
+		i = --i - i--; // i = 1 - 1 = 0
 		
 		System.out.println("Third static block i after operation " + i);
 	}
@@ -42,8 +42,8 @@ class B1 extends A1 {
 		System.out.println("Forth normal block i before operation " + i);
 		System.out.println("Forth normal block j before operation " + j);
 		
-		i = ++i + i++;
-		j = ++j + j++;
+		i = ++i + i++; // i = 3 + 3 = 6
+		j = ++j + j++; // j = 403 + 403 = 806
 		
 		System.out.println("Forth normal block i after operation " + i);
 		System.out.println("Forth normal block j after operation " + j);
